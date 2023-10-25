ad7e1e511d586ec1fcc1ad9e3693e1981888b75e
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2023-10-25 17:42:55 +0800

    Add files via upload

---
+++ compound statement/compound statement/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  compound statement
+//
+//  Created by Nisha Ramprasath on 23/10/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int sum=10;
+    int x=5;
+    sum+=x;
+    cout<<sum;
+    return 0;
+}
